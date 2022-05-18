d90d06add40a9f9b3bbc98958f6b1b61487fad13
William Dias Marcos <william.diasmarcos@gmail.com>
2022-05-17 22:15:05 -0300

    Part 01

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// css
+import './App.css'
+
+// categories
+import categories from './API/api';
+// components
+import Row from './components/Row'
+
+function App() {
+  return <div className="App">
+    {/*Navbar,
+      destaque,
+    */}
+    {categories.map((category)=>{
+      return (
+        <Row 
+          key={category.name} 
+          title={category.title} 
+          path={category.path}
+          isLarge={category.isLarge}
+        />
+      );
+    })}
+  </div>
+}
+
+export default App;
---
+++ src/components/Row.jsx
@@ -0,0 +1,47 @@
+// react
+import { useEffect, useState } from "react";
+// css
+import styles from "./Row.module.css";
+// api
+import { getMovies } from "../API/api";
+
+const imageHost = "https://image.tmdb.org/t/p/original/"
+
+function Row({ title, path, isLarge }){
+
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async (_path)=>{
+    try{
+      const data = await getMovies(_path);
+      // console.log("data ", data)
+      setMovies(data?.results);
+    } catch (error){
+        console.log("fetchMovies error:", error)
+    }
+  };
+
+  useEffect(()=>{
+    fetchMovies(path);
+  }, [path])
+
+  return(                    
+    <div className={styles.container}>
+      <h2 className={styles.header}>{title}</h2>
+      <div className={styles.cards}>
+        {movies?.map(movie=>{
+          return(
+            <img 
+              className={ isLarge ? styles.movieLarge : styles.movie}
+              key={movie.id} 
+              src={`${imageHost}${movie.poster_path}`} 
+              alt={movie.name} 
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Row;
